28d0be471556bde89adc600a6d3794c17a0975c4
Ruben <rdario45@gmail.com>
2019-08-27 01:30:31 -0500

    Create first database version. - tables users, sessions, companies and projects.

--- backend/conf/evolutions/default/1.sql
+++ backend/conf/evolutions/default/1.sql
@@ -1,11 +1,44 @@
 # --- Creation of events table
 
 # --- !Ups
-CREATE TABLE empresas (
-	id serial NOT NULL,
-	nombre varchar(100) NOT NULL
+
+CREATE TABLE users (
+	email varchar(100) NOT NULL,
+	"password" varchar(200) NOT NULL,
+	state varchar(100) NULL,
+	CONSTRAINT users_pkey PRIMARY KEY (email)
 );
-COMMENT ON TABLE empresas IS 'Almacena los empresas.';
+
+CREATE TABLE sessions (
+	id serial NOT NULL,
+	"user" varchar(100) NOT NULL,
+	token varchar(200) NOT NULL,
+	timestamp numeric(12,2) NOT NULL,
+	CONSTRAINT sessions_pkey PRIMARY KEY (id),
+	CONSTRAINT sessions_user_fkey FOREIGN KEY ("user") REFERENCES users(email)
+);
+
+CREATE TABLE companies (
+	id serial NOT NULL,
+	name varchar(100) NOT NULL,
+	admin varchar(100) NOT NULL,
+	CONSTRAINT companies_pkey PRIMARY KEY (id),
+	CONSTRAINT companies_admin_fkey FOREIGN KEY (admin) REFERENCES users(email)
+);
+
+CREATE TABLE projects (
+	id serial NOT NULL,
+	name varchar(100) NOT NULL,
+	description varchar(200) NOT NULL,
+	cost numeric(12,2) NOT NULL,
+	company int4 NOT NULL,
+	CONSTRAINT projects_pkey PRIMARY KEY (id),
+	CONSTRAINT projects_company_fkey FOREIGN KEY (company) REFERENCES companies(id)
+);
+
 
 # --- !Downs
-drop table empresas;
+DROP TABLE public.projects;
+DROP TABLE public.companies;
+DROP TABLE public.sessions;
+DROP TABLE public.users;
