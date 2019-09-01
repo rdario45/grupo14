551f25536ba7837f5edade123c6ccae81ac2c64d
datorres58 <da.torres58@uniandes.edu.co>
2019-09-01 17:30:05 -0500

    Actualización de consulta (estado del diseño como un enum)

--- backend/conf/evolutions/default/1.sql
+++ backend/conf/evolutions/default/1.sql
@@ -43,12 +43,12 @@ CREATE TABLE design (
 
 
 
-insert into design (id, email, resized, originalPath)
+insert into design (id, email,  originalPath)
 values
-(1,'diegoatorres@gmail.com','/home/diego/Imágenes/Captura de pantalla de 2019-08-27 21-39-35.png'),
-(2,'da.torres58@uniandes.edu.co','/home/diego/Imágenes/Captura de pantalla de 2019-08-27 21-40-26.png'),
-(3,'ruben@uniandes.edu.co','/home/diego/Imágenes/Captura de pantalla de 2019-08-27 21-40-27.png'),
-(4,'camilo@uniandes.edu.co','/home/diego/Imágenes/Captura de pantalla de 2019-08-27 21-40-36.png');
+(1,'diegoatorres@gmail.com',  '/home/diego/Imágenes/Captura de pantalla de 2019-08-27 21-39-35.png'),
+(2,'da.torres58@uniandes.edu.co', '/home/diego/Imágenes/Captura de pantalla de 2019-08-27 21-40-26.png'),
+(3,'ruben@uniandes.edu.co',  '/home/diego/Imágenes/Captura de pantalla de 2019-08-27 21-40-27.png'),
+(4,'camilo@uniandes.edu.co',  '/home/diego/Imágenes/Captura de pantalla de 2019-08-27 21-40-36.png');
 
 # --- !Downs
 DROP TABLE projects;
