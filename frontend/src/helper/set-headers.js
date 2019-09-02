e7917e90c059d4d503f5767ac64d1bce84d9e4dd
Juan Camilo Colmenares Rocha <jc.colemenares@uniandes.edu.co>
2019-09-02 00:35:41 -0500

    Include service enterprise

--- frontend/src/helper/set-headers.js
+++ frontend/src/helper/set-headers.js
@@ -1,5 +1,12 @@
-export function setHeader() {
-    var headers = new Headers();
+export class CustomHeader {
+    getAuthorization() {
+        var headers = this.getWithoutAuthorization();
         headers.append('Authorization', `${localStorage.getItem('token')}`);
+        return headers;
+    }
+    getWithoutAuthorization() {
+        var headers = new Headers();
         headers.append('Content-Type', 'application/json');
+        return headers;
+    }
 }
