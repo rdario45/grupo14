28dcb994e431282de4bf85aa19d2c84a01fe0318
Ruben Fernandez <rubenfernandez@seven4n.com>
2019-09-09 01:50:13 -0500

    Add env config.

--- frontend/src/services/Enterprise.js
+++ frontend/src/services/Enterprise.js
@@ -9,7 +9,7 @@ export class EnterpriseService {
             headers: header.getWithoutAuthorization(),
             cache: 'default',
         };
-        return fetch(`/api/exists-account/${email}`, requestOptions);
+        return fetch(`${process.env.REACT_APP_BASE_API}/api/exists-account/${email}`, requestOptions);
     }
     create(enterprise) {
         const requestOptions = {
@@ -18,7 +18,7 @@ export class EnterpriseService {
             cache: 'default',
             body: JSON.stringify(enterprise)
         };
-        return fetch(`/api/create-account`, requestOptions)
+        return fetch(`${process.env.REACT_APP_BASE_API}/api/create-account`, requestOptions)
     }
 }
 
